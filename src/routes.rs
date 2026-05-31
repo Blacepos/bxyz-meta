6dfbf39c6316934a8073e9edea4b750e32685ce7
Blacepos <jraustin999@gmail.com>
2026-05-30 23:32:38 -0500

    Add music sorting

--- src/routes.rs
+++ src/routes.rs
@@ -1,4 +1,4 @@
-use std::path::Path;
+use std::{cmp::Ordering, fs::DirEntry, path::Path};
 
 use axum::{
     extract::State,
@@ -35,15 +35,7 @@ pub async fn route_audio(State(tera_eng): State<tera::Tera>) -> (StatusCode, Res
     for res in std::fs::read_dir(AUDIO_DIR).expect("Audio directory exists") {
         match res {
             Ok(entry) => {
-                let file_name = entry
-                    .file_name()
-                    .into_string()
-                    .expect("I won't make the filenames weird");
-
-                files.push(MusicDumpFile {
-                    name: file_name.clone(),
-                    path: format!("{AUDIO_ROUTE}/{file_name}"),
-                });
+                files.push(entry);
             }
             Err(e) => {
                 log::error!(
@@ -54,7 +46,25 @@ pub async fn route_audio(State(tera_eng): State<tera::Tera>) -> (StatusCode, Res
         }
     }
 
-    tera_ctx.insert("files", &files);
+    files.sort_by(compare_dir_entries);
+
+    let template_structs: Vec<MusicDumpFile> = files
+        .iter()
+        .map(|d| {
+            let file_name = d
+                .file_name()
+                .into_string()
+                .expect("I won't make the filenames weird, I promise");
+
+            MusicDumpFile {
+                name: file_name.clone(),
+                path: format!("{AUDIO_ROUTE}/{file_name}"),
+            }
+        })
+        .rev() // Reverse, so "larger" entries (newer) are first
+        .collect();
+
+    tera_ctx.insert("files", &template_structs);
 
     match tera_eng.render("music_dump.html", &tera_ctx) {
         Ok(html) => (StatusCode::OK, Html(html).into_response()),
@@ -65,5 +75,20 @@ pub async fn route_audio(State(tera_eng): State<tera::Tera>) -> (StatusCode, Res
                 "Failed to display page".into_response(),
             )
         }
+    }
+}
+
+fn compare_dir_entries(a: &DirEntry, b: &DirEntry) -> Ordering {
+    let a_maybe_created = a.metadata().and_then(|m| m.created()).ok();
+    let b_maybe_created = b.metadata().and_then(|m| m.created()).ok();
+
+    match (a_maybe_created, b_maybe_created) {
+        (Some(a_created), Some(b_created)) => Ord::cmp(&a_created, &b_created),
+        // Files with bad metadata are "older" (will appear at the end of the list)
+        (Some(_), None) => Ordering::Greater,
+        (None, Some(_)) => Ordering::Less,
+        // Both have bad metadata, sort alphabetically.
+        // A "lower" name (i.e., alphabetically first), should be "newer"/greater
+        (None, None) => Ord::cmp(&a.file_name(), &b.file_name()).reverse(),
     }
 }
